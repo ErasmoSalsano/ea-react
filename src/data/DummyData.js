822063a06ad0b0785f02284a29011f46b2e4125e
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-11-04 02:24:58 +0100

    chore: update DummyData and DataArticle

--- src/data/DummyData.js
+++ src/data/DummyData.js
@@ -9,8 +9,19 @@ export const TabButtons = {
         { id: 7, nome:'Battlefield', categoria: 'Battlefield 2042' },
         { id: 8, nome:'Inside Ea', categoria: 'Inside EA' }],
     insideEa:[
-        { id: 1, nome:'Le storie', categoria: 'storie' },
-        { id: 2, nome:'Diversità e Inclusione', categoria: 'diversità' },
-        { id: 3, nome:'Persone e Cultura', categoria: 'cultura' },
-        { id: 4, nome:'Investire nella Comunità', categoria: 'comunità' }]
+        { id: 1, nome:'Le storie', categoria: 'Storie' },
+        { id: 2, nome:'Diversità e Inclusione', categoria: 'Diversità' },
+        { id: 3, nome:'Persone e Cultura', categoria: 'Cultura' },
+        { id: 4, nome:'Investire nella Comunità', categoria: 'Comunità' }],
+    eaSports:[
+        { id: 1, nome:'FIFA', categoria: 'FIFA' },
+        { id: 2, nome:'Madden NFL™', categoria: 'MADDEN NFL', void: true},
+        { id: 3, nome:'F1', categoria: 'F1® 22' },
+        { id: 4, nome:'NHL', categoria: 'NHL', void: true},
+        { id: 5, nome:'UFC', categoria: 'UFC' },
+        { id: 6, nome:'NBA LIVE', categoria: 'NBA LIVE' },
+        { id: 7, nome:'FIFA Mobile', categoria: 'FIFA Mobile' },
+        { id: 8, nome:'Madden NFL Mobile', categoria: 'Madden NFL Mobile', void: true},
+        { id: 9, nome:'NBA LIVE Mobile', categoria: 'NBA LIVE Mobile' }
+    ]
 }
