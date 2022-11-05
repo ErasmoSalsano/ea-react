e9fe9db1eeb47ae0f24381ed6941160c92d1a36e
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-11-05 02:51:09 +0100

    feat: add new component pages/Manteinance

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import MainFrame from "./components/MainFrame";
 import { Home } from "./pages/Home";
-
-
+import { Manteinance } from "./pages/Manteinance";
 
 export function App() {
   return (
@@ -13,8 +12,8 @@ export function App() {
           {/* <Route path="path" element={element} /> */}
         </Route>
         {/* <Route path='/shop' element={<Shop/>}/> */}
+        <Route path="*" element={<Manteinance/>} />
       </Routes>
-       
     </div>
   );
 }
