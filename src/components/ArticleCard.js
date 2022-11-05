630ca57550ad540aa3b7c262614646d5126cacc2
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-11-05 02:48:48 +0100

    refactoring: update ArticleCard

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,17 +1,17 @@
 import { useEffect } from "react"
+import { Link } from "react-router-dom"
 
 export function ArticleCard({ article }) {
 
     useEffect(() => {}, [article])
     return (
-        <a href="sitooriginale.it" className="article-card">
+        <Link to={article.link !== ''?article.link:'/maintenance'} className="article-card">
             <img src={article.image} alt={article.imageDesc} />
             <div className="article-text">
-                <div><span>{article.category}</span><span className="normal">{article.date}</span></div>
+                {article.category && article.date && <div><span>{article.category}</span><span className="normal">{article.date}</span></div>}
                 <h3>{article.title}</h3>
-                <p>{article.desc}</p>
+                {article.desc && <p>{article.desc}</p>}
             </div>
-        </a>
-        
+        </Link>
     )
 }
