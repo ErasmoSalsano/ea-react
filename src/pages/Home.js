1e8bb8420b8b6be931f6c2d87b2749e550bd9db5
giuseppe filannino <peppe.f94@hotmail.it>
2022-11-08 11:23:06 +0100

    fix: general changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -49,7 +49,7 @@ export function Home() {
         mobile="/assets/images/hero/home/hero2/hero-767-medium-console-7x2-xl.jpg.adapt.crop3x5.767w.jpg"
         tablet768="/assets/images/hero/home/hero2/hero-1023-medium-console-7x2-xl.jpg.adapt.crop2x1.1023w.jpg"
         tablet1024="/assets/images/hero/home/hero2/hero-1455-medium-console-7x2-xl.jpg.adapt.crop5x2.1455w.jpg"
-        desktop="/assets/images/hero/home/hero2/hero-1920-medium-console-7x2-xl.jpg.adapt.crop7x2.1920w.jpg"
+        desktop="/assets/images/hero/home/hero2/hero-1455-medium-console-7x2-xl.jpg.adapt.crop5x2.1455w.jpg"
         spanText="Non limitarti al gioco. Gioca al massimo. Sblocca ricompense esclusive, contenuti per i soli
         membri e una libreria dei migliori titoli."
         buttonSpan='Iscriviti subito'
