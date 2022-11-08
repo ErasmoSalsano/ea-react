e0ec95b1b508a3856e3f29ec92b95360a7189b0b
giuseppe filannino <peppe.f94@hotmail.it>
2022-11-08 18:14:54 +0100

    fix:erase error

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import MainFrame from "./components/MainFrame";
+import { Home } from "./pages/Home";
+import { News } from "./pages/News";
+import { Manteinance } from "./pages/Manteinance";
+import { InsideEa } from "./pages/InsideEa";
+
+export function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<MainFrame/>}>
+          <Route index element={<Home/>} />
+          <Route path="/informazioni/notizie" element={<News/>} />
+          <Route path="/informazioni/inside-ea" element={<InsideEa/>} />
+          {/* <Route path="path" element={element} /> */}
+        </Route>
+        {/* <Route path='/shop' element={<Shop/>}/> */}
+        <Route path="*" element={<Manteinance/>} />
+      </Routes>
+    </div>
+  );
+}
