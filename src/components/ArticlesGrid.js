d99258ad6651131ce9c92cf6faba3136053bc756
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-11-18 13:15:37 +0100

    feat: add new Component ButtonLoadMore

--- src/components/ArticlesGrid.js
+++ src/components/ArticlesGrid.js
@@ -2,9 +2,11 @@ import { ArticleCard } from "./ArticleCard";
 import { useEffect } from "react";
 import { MainWrap } from "./0_wraps/mainWrap";
 import { MainContentWrap } from "./0_wraps/mainContentWrap";
+import { ButtonLoadMore } from "./Buttons/ButtonLoadMore";
 
 export function ArticlesGrid(props) {
     let myNewData = data(props.request, props.amount, props.argument)
+    let value
 
     useEffect(()=>{
         data()
@@ -28,17 +30,7 @@ export function ArticlesGrid(props) {
                     </div>
                 </MainContentWrap>
                 :props.button === 'B'?
-                <MainContentWrap p>
-                    <div className="large-details btn">
-                        <div className="l-details">
-                            <svg>
-                                <path d="M34.5,25.5h-6v-6c0-0.6-0.4-1-1-1 c-0.6,0-1,0.4-1,1v6h-6c-0.6,0-1,0.4-1,1c0,0.6,0.4,1,1,1h6v6c0,0.6,0.4,1,1,1c0.6,0,1-0.4,1-1v-6h6c0.6,0,1-0.4,1-1 C35.5,25.9,35.1,25.5,34.5,25.5z"></path>
-                                <path d="M27,54C12.1,54,0,41.9,0,27S12.1,0,27,0s27,12.1,27,27S41.9,54,27,54z M27,2 C13.2,2,2,13.2,2,27s11.2,25,25,25s25-11.2,25-25S40.8,2,27,2z"></path>
-                            </svg>
-                            <h2>Carica altro</h2>
-                        </div>
-                    </div>
-                </MainContentWrap>
+                <ButtonLoadMore onClick={()=> console.log(data('all', value = props.amount + 6, props.argument))}/>
                 :null}
             </MainContentWrap>
         </MainWrap>
